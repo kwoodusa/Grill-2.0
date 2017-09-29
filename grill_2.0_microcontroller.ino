cef8479fe0175d177adce461e7560add9ca1d588
kwoodusa <32208659+kwoodusa@users.noreply.github.com>
2017-09-29 17:28:39 -0500

    final monitor and controller code with mit appinventor

--- grill_2.0_microcontroller.ino
+++ grill_2.0_microcontroller.ino
@@ -8,8 +8,8 @@
 #include <WiFi101.h>//WiFi library for winc1500
 
 //wifi variables
-char ssid[] = "NETGEAR63";
-char pass[] = "tinyskates469";
+char ssid[] = "network";
+char pass[] = "password";
 int status = WL_IDLE_STATUS; //status of wifi
 WiFiServer server(80);//declare server object and specify port
 
@@ -23,8 +23,6 @@ int lowTemp = highTemp - 3;
 int grillTemp = 0.0;
 int meatTemp = 0.0;
 boolean isCelcius = false;
-unsigned long tempTimer = 0.0;
-int timeOfDelay = 20000;// 20 seconds
 
 void setup() {
   //initialize wifi pins
@@ -40,35 +38,35 @@ void setup() {
   // set analog voltage reference 0 to 5 volts
   analogReference(DEFAULT);
   // starting serial communications and set baud to 9600
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  Serial.println("started serial");
+  //    Serial.begin(9600);
+  //    while (!Serial) {
+  //      ; // wait for serial port to connect. Needed for native USB port only
+  //    }
+  //  Serial.println("started serial");
   // check for the presence of the shield:
   if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present");
+    //Serial.println("WiFi shield not present");
     // don't continue:
     while (true);
   }
-  Serial.print("wifi shield present! ");
+  //Serial.println("wifi shield present! ");
   // connect to wifi network
   while (status != WL_CONNECTED) {
-    Serial.println("attempting to connect to wifi");
+    //Serial.println("attempting to connect to wifi");
     // connect to WPA/WPA2
     status = WiFi.begin(ssid, pass);
     //give network time to connect
     delay(10000);//wait 10 seconds
   }
-    Serial.println("connected to wifi");
+  //Serial.println("connected to wifi");
   server.begin();
-    Serial.println("server started");
+  //Serial.println("server started");
 }
 
 void loop() {
 
   grillTimer();// run grill controller/monitor code
- 
+
   WiFiClient client = server.available(); // listen for incoming clients
 
   if (client) {                 // if you get a client
@@ -80,17 +78,19 @@ void loop() {
         if (c == '\n') {        // if the byte is a new line character
           // if the current line is blank, you got two newline characters in a row.
           // that's the end of hte client HTTP reqeust, so send a response:
-          if (currentLine.length() == 0){
+          if (currentLine.length() == 0) {
             // HTTP headers always start with a response code (HTTP/1.1 200 OK)
             // and content-type so the client knows what's coming, then a blank line:
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println("");
             //HTTP response body
-            client.print("Grill temp is ");
-            client.print(grillTemp);
-            client.print("Meat temp is ");
-            client.print(meatTemp);
+            client.print("Grill temp = ");
+            client.println(grillTemp);
+            client.print("Meat temp = ");
+            client.println(meatTemp);
+            client.print("High temp = ");
+            client.println(highTemp);
             // HTTP response end
             client.println();
             // get out of the while loop
@@ -103,62 +103,57 @@ void loop() {
         }
 
         // check the client request
-        if (currentLine.endsWith("GET /C")){
+        if (currentLine.endsWith("/C")) {
           isCelcius = true;
+          //          Serial.println("degrees set to C");
+          //          Serial.println(currentLine);
         }
-        if (currentLine.endsWith("GET /F")){
+        if (currentLine.endsWith("/F")) {
           isCelcius = false;
+          //          Serial.println("degrees set to F");
+          //          Serial.print("high temp is ");
+          //          Serial.println(highTemp);
+        }
+        if (currentLine.endsWith("/T")) {
+          int n = currentLine.length();
+          String t = (String)currentLine.charAt(n - 5) + (String)currentLine.charAt(n - 4) + (String)currentLine.charAt(n - 3);
+          if (isCelcius) {
+            highTemp = (t.toInt() * 1.8) + 32;
+          } else {
+            highTemp = t.toInt();
+          }
+          //          Serial.print("current line ");
+          //          Serial.println(currentLine);
+          //          Serial.print("temp sent is ");
+          //          Serial.println(t);
+          //          Serial.println(highTemp);
         }
 
       }
     }
   }
-  //serialPrint();
 }
 
 void grillTimer() {
-  // turn on green led on proto-board
-  digitalWrite(led, HIGH);
-  
+
   // analogRead() gets the temp from the ADC on the UNO
   // then turn the digital value into a readable value
   grillTemp = (int)((analogRead(A1) * 1.76) - 418);
   meatTemp = (int)((analogRead(A3) * 1.76) - 418);
-  
-  // millis() returns the time in milli-seconds
-  // since the board began running the current program.
-  // millis() resets to zero after 50 days
-  unsigned long t = millis() - tempTimer;
 
-  if (t > timeOfDelay) {      // if it has been n seconds since last monitor
-    // average a system monitor n/60 times a second
-    tempTimer += timeOfDelay; // add n seconds onto old value
-
-    if (grillTemp < lowTemp) {
-      // turn fan on
-      digitalWrite(fan, HIGH);
-    } else if (grillTemp > highTemp) {
-      // turn fan off
-      digitalWrite(fan, LOW);
-    }
+  if (grillTemp < lowTemp) {
+    // turn fan on
+    digitalWrite(fan, HIGH);
+    // turn on green led on proto-board
+    digitalWrite(led, HIGH);
   }
-  // turn off green led on proto-boards
-  // digitalWrite(led, LOW);
-}
-
-void setTemp(int temp) {
-  if (isCelcius) {
-    highTemp = (temp * 1.8) + 32;
-  } else {
-    highTemp = temp;
+  if (grillTemp > highTemp) {
+    // turn fan off
+    digitalWrite(fan, LOW);
+    // turn off green led on proto-boards
+    digitalWrite(led, LOW);
   }
 }
 
-//void serialPrint() {
-//  Serial.print("calculated temp 1 ");
-//  Serial.println(grillTemp);
-//  Serial.print("calculated temp 2 ");
-//  Serial.println(meatTemp);
-//}
 
 
